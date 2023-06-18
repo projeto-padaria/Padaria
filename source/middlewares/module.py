96421df81b3c271ed1250eb5f96f8092fffe37c1
RMCSa <rafamoca42@gmail.com>
2023-06-18 00:19:42 -0300

    sla

--- source/middlewares/module.py
+++ source/middlewares/module.py
@@ -5,6 +5,7 @@ from os import system as clearTerminal
 
 # Importação da Classe debug
 from debug import libDebug
+
 debug = libDebug()
 
 
@@ -56,29 +57,46 @@ class Connect:
 
     def insertTableFun(self, valores) -> None:
         aux = 0
-        for indice,valor in enumerate(valores):
-            if indice in (0,1,2,3,4,5,6) and valor == '':
+        for indice, valor in enumerate(valores):
+            if indice in (0, 1, 2, 3, 4, 5, 6) and valor == "":
                 aux += 1
             else:
-                if valor == '':
+                if valor == "":
                     valores.pop(indice)
-                    valores.insert(indice,None)
+                    valores.insert(indice, None)
         if aux == 0:
             self.cursor.execute(
-                "INSERT INTO padaria.endereco VALUES (?,?,?,?,?,?)",(valores[9],valores[7],valores[8],valores[10],valores[11],valores[12])
+                "INSERT INTO padaria.endereco VALUES (?,?,?,?,?,?)",
+                (
+                    valores[9],
+                    valores[7],
+                    valores[8],
+                    valores[10],
+                    valores[11],
+                    valores[12],
+                ),
             )
             self.cursor.execute(
-                "INSERT INTO padaria.funcionario VALUES (?,?,?,?,?,?,?, NULL)",(valores[0],valores[1],valores[2],valores[3],valores[4],valores[5],valores[6])
+                "INSERT INTO padaria.funcionario VALUES (?,?,?,?,?,?,?, NULL)",
+                (
+                    valores[0],
+                    valores[1],
+                    valores[2],
+                    valores[3],
+                    valores[4],
+                    valores[5],
+                    valores[6],
+                ),
             )
             self.cursor.commit()
             debug.printSuccess("Funcionário cadastrado com sucesso!!")
         else:
             raise Exception("Prencha os campos Obrigatórios Adequadamente!!")
-        
 
     def showTableFun(self, tableWidget):
-        comando_SQL = "SELECT F.cpf,F.nome,F.sobrenome,F.senha,F.cargo,F.salario,F.telefone,E.bairro,E.rua,E.numero,E.cidade,E.UF,E.cep FROM padaria.funcionario F,padaria.endereco E WHERE F.idEndereco = E.idEndereco;"
-        self.cursor.execute(comando_SQL)
+        self.cursor.execute(
+            "SELECT F.cpf,F.nome,F.sobrenome,F.senha,F.cargo,F.salario,F.telefone,E.bairro,E.rua,E.numero,E.cidade,E.UF,E.cep FROM padaria.funcionario F,padaria.endereco E WHERE F.idEndereco = E.idEndereco;"
+        )
         dados_lidos = self.cursor.fetchall()
         table = tableWidget
         try:
@@ -94,9 +112,10 @@ class Connect:
         self.cursor.execute(f"DELETE FROM padaria.funcionario WHERE cpf = '{cpf}' ")
         self.cursor.commit()
         return "Cadastro de Funcionário excluido com sucesso!"
-        
-    def updateTable(self,fullDataSet):
-        self.cursor.execute(f"""
+
+    def updateTable(self, fullDataSet):
+        self.cursor.execute(
+            f"""
         UPDATE padaria.endereco 
         SET
             bairro = '{fullDataSet[7]}',
@@ -107,8 +126,10 @@ class Connect:
             cep = '{fullDataSet[12]}'
         FROM padaria.endereco AS E
         INNER JOIN padaria.funcionario AS F ON E.idEndereco = F.idEndereco
-        WHERE F.cpf = '{fullDataSet[0]}'""")
-        self.cursor.execute(f""" UPDATE padaria.funcionario SET
+        WHERE F.cpf = '{fullDataSet[0]}'"""
+        )
+        self.cursor.execute(
+            f""" UPDATE padaria.funcionario SET
 
             cpf = '{fullDataSet[0]}',
             nome = '{fullDataSet[1]}',
@@ -117,8 +138,10 @@ class Connect:
             cargo = '{fullDataSet[4]}',
             salario = {fullDataSet[5]},
             telefone = {fullDataSet[6]}
-            WHERE cpf = '{fullDataSet[0]}'""")
+            WHERE cpf = '{fullDataSet[0]}'"""
+        )
         self.cursor.commit()
+
 
 # if __name__ == "__main__":
 #     a = Connect("BD23333", "BD23333")
